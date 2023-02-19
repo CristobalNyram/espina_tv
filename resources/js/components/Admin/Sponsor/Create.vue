16606bdc77f5b0b198cd2bf3648b823de29354f6
cristobal nyram <cristobalmarincautiva@gmail.com>
2023-02-19 16:56:51 -0600

    modal for add

--- resources/js/components/Admin/Sponsor/Create.vue
+++ resources/js/components/Admin/Sponsor/Create.vue
@@ -1,17 +1,65 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
 
-                    <div class="card-body">
-                        I'm an example component.
+    <div>
+
+        <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#add-sponsor">
+            <span>Add sponsor</span>  <i class="bi bi-plus-circle-fill"></i>
+        </button>
+
+
+
+  <!-- Modal -->
+            <div class="modal fade" id="add-sponsor" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">ADD SPONSOR</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Name:</label>
+                            <input type="text" class="form-control" id="recipient-name">
+                        </div>
+
+
+                        <div class="mb-3">
+                            <label for="message-text" class="col-form-label">Time support:</label>
+                            <input type="text" class="form-control" id="recipient-name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="message-text" class="col-form-label">Phone number:</label>
+                            <textarea class="form-control" id="message-text"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="message-text" class="col-form-label">Address:</label>
+                            <input type="text" class="form-control" id="recipient-name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="message-text" class="col-form-label">Email:</label>
+                            <input type="text" class="form-control" id="recipient-name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="message-text" class="col-form-label">Description:</label>
+                            <textarea class="form-control" id="message-text"></textarea>
+                        </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-primary">Create </button>
                     </div>
                 </div>
+                </div>
             </div>
-        </div>
+
+
     </div>
+
+
+
+
 </template>
 
 <script>
--- resources/views/admin/sponsor/index.blade.php
+++ resources/views/admin/sponsor/index.blade.php
@@ -5,8 +5,21 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Sponsors') }}</div>
+                <div class="card-header">{{ __('Sponsors') }}   <i class="fas fa-spinner fa-spin"></i>
+                </div>
 
+
+
+                <div class="d-flex justify-content-end  pb-1 pt-1 ">
+
+                    <div class='m-2'>
+                        <div id="app">
+                            <create-sponsors/>
+                        </div>
+
+                    </div>
+
+                </div>
                 <div class="card-body">
                     <div id="app">
                         <index-sponsors/>
@@ -15,9 +28,11 @@
 
 
                 </div>
+
             </div>
         </div>
     </div>
 </div>
+
 
 @endsection
